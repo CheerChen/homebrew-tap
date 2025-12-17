a9bbb8047ad5d9af9bcc5d790284e487673b8878
CheerChen <cheerchen37@gmail.com>
2025-12-17 14:27:41 +0900

    Update pr-dump.rb

--- Formula/pr-dump.rb
+++ Formula/pr-dump.rb
@@ -2,8 +2,8 @@
 class PrDump < Formula
   desc "Dump GitHub PR context (metadata, comments, diffs) for LLM review"
   homepage "https://github.com/CheerChen/pr-dump"
-  url "https://github.com/CheerChen/pr-dump/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "e3062ef5c531ea6b90daa85f1e6dd1413ea2931cb9daa4d89cd64378c379b91b"
+  url "https://github.com/CheerChen/pr-dump/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "848486830d2ae1446294b03051ca7363ca15a75931a0f4f06b4a1127e7137de5"
   license "MIT"
 
   depends_on "gh"
